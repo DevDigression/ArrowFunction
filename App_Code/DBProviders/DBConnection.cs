42220d8b016ebaddc7984eb62afac491620965ef
Hugo Cardet <hcardet@hotmail.com>
2018-02-24 16:56:55 -0500

    DB Providers

---
+++ App_Code/DBProviders/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+
+
+    public DBConnection()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public string get_markers(int provider_id) {
+        using (var db = new PetaPoco.Database("linkSTEM"))
+        {
+            var SQL = Sql.Builder.Append("SELECT * ")
+            .Append("FROM [linkSTEM].[dbo].[marker]")
+            .Append("WHERE [provider_id] = @0", provider_id);
+            return JsonConvert.SerializeObject(db.Fetch<Marker>(SQL));
+        }
+
+}
--- default.aspx.cs
+++ default.aspx.cs
@@ -8,9 +8,15 @@ using System.Xml;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public DBConnection connection { get; set; }
 
     protected void Page_Load(object sender, EventArgs e)
     {
+       this.connection = new DBConnection();
+    }
 
+    [WebMethod]
+    public string get_users(int provider_id) {
+        return this.connection.get_markers(provider_id);
     }
 }
