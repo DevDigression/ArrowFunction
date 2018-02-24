76d41e90fa1a495b2e7026f6f07603dba2510f74
sam.j.maclaren <sam.j.maclaren@gmail.com>
2018-02-24 14:57:00 -0500

    Add marker, provider class

---
+++ App_Code/Models/Marker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Marker
+/// </summary>
+public class Marker
+{
+    public int ID { get; set; }
+
+    public int ProviderID { get; set; }
+
+    public float Latitude { get; set; }
+
+    public float Longitude { get; set; }
+}
--- App_Code/Models/Provider.cs
+++ App_Code/Models/Provider.cs
@@ -4,15 +4,16 @@ using System.Collections.Generic;
 /// <summary>
 /// School Model
 /// </summary>
-public class School
+public class Provider
 {
     public int ID { get; set; }
 
-    public string Name { get; set; }
+    public string ProviderName { get; set; }
 
     public string Username { get; set; }
 
     public string Password { get; set; }
 
-    public List<Professional> Professionals { get; set; }
+    public List<Marker> Markers { get; set; }
+
 }
