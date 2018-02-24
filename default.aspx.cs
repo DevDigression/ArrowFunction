85e8ccecff88a1a792c47cfbbe5a215b4b20d2a6
sam.j.maclaren <sam.j.maclaren@gmail.com>
2018-02-24 13:27:28 -0500

    Created new user and profile controllers.

--- default.aspx.cs
+++ default.aspx.cs
@@ -8,7 +8,18 @@ using System.Xml;
 
 public partial class _Default : System.Web.UI.Page
 {
-    
+    [WebMethod]
+    public string addNewSchool(School school)
+    {
+        return "";
+    }
+
+    [WebMethod]
+    public string getSchool(string username)
+    {
+        return "";
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
