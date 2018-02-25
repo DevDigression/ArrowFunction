cba1b4f1d3f706c928e0cd643acdaef7eaefcfea
Hugo Cardet <hcardet@hotmail.com>
2018-02-24 22:01:38 -0500

    DB Provider updated

--- App_Code/DBProviders/DBConnection.cs
+++ App_Code/DBProviders/DBConnection.cs
@@ -8,11 +8,11 @@ using System.Web;
 /// <summary>
 /// Summary description for DBConnection
 /// </summary>
-public class DBConnection
+public class DBProvider
 {
 
 
-    public DBConnection()
+    public DBProvider()
     {
         //
         // TODO: Add constructor logic here
@@ -31,5 +31,92 @@ public class DBConnection
         Marker test_marker = new Marker();
         test_marker.Description = "Hello World";
         return JsonConvert.SerializeObject(test_marker);
+    }    
+    public string getAllMarkers()
+    {
+        using (var db = new PetaPoco.Database("linkSTEM"))
+        {
+            var SQL = Sql.Builder.Append("SELECT *")
+                                    .Append("FROM [dbo].[Markers]");
+
+            return JsonConvert.SerializeObject(db.Fetch<Marker>(SQL));
+        }
+
+    }
+
+    public bool deleteMarker(Marker marker)
+    {
+        try
+        {
+            using (var db = new PetaPoco.Database("linkSTEM"))
+            {
+                db.Insert("Markers", "marker_ID", marker);
+                return true;
+            }
+
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public bool addMarker(Marker marker)
+    {
+        try
+        {
+            using (var db = new PetaPoco.Database("linkSTEM"))
+            {
+                db.Insert("Markers", "marker_ID", marker);
+                return true;
+            }
+
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public bool addProvider(Provider provider)
+    {
+        try
+        {
+            using (var db = new PetaPoco.Database("linkSTEM"))
+            {
+                db.Insert("Providers", "provider_ID", provider);
+                return true;
+            }
+
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+    }
+
+    public string getProvider(string username, string password)
+    {
+        using (var db = new PetaPoco.Database("linkSTEM"))
+        {
+            var SQL = Sql.Builder.Append("SELECT *")
+                                 .Append("FROM [dbo].[Providers]")
+                                 .Append("WHERE [provider_Username] = @0 AND [provider_Password] = @1", username, password);
+
+            return JsonConvert.SerializeObject(db.FirstOrDefault<Provider>(SQL));
+        }
+    }
+
+    public string getProviderMarkers(int providerID)
+    {
+        using (var db = new PetaPoco.Database("linkSTEM"))
+        {
+            var SQL = Sql.Builder.Append("SELECT *")
+                                 .Append("FROM [dbo].[Markers]")
+                                 .Append("WHERE [provider_ID] = @0", providerID);
+
+            return JsonConvert.SerializeObject(db.Fetch<Marker>(SQL));
+        }
     }
 }
--- default.aspx.cs
+++ default.aspx.cs
@@ -10,99 +10,46 @@ using System.Xml;
 
 public partial class _Default : System.Web.UI.Page
 {
+
+    public DBProvider dbProvider { get; set; }
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        this.dbProvider = new DBProvider();
     }
 
     [WebMethod]
-    public static string getAllMarkers() {
-        using (var db = new PetaPoco.Database("linkSTEM"))
-        {
-            var SQL = Sql.Builder.Append("SELECT *")
-                                    .Append("FROM [dbo].[Markers]");
-
-            return JsonConvert.SerializeObject(db.Fetch<Marker>(SQL));
-        }
-
+    public string getAllMarkers() {
+        return this.dbProvider.getAllMarkers();
     }
 
     [WebMethod]
     public bool deleteMarker(Marker marker)
     {
-        try
-        {
-            using (var db = new PetaPoco.Database("linkSTEM"))
-            {
-                db.Insert("Markers", "marker_ID", marker);
-                return true;
-            }
-
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        return this.dbProvider.deleteMarker(marker);
     }
 
     [WebMethod]
     public bool addMarker(Marker marker)
     {
-        try
-        {
-            using (var db = new PetaPoco.Database("linkSTEM"))
-            {
-                db.Insert("Markers", "marker_ID", marker);
-                return true;
-            }
-
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        return this.dbProvider.addMarker(marker);
     }
 
     [WebMethod]
     public bool addProvider(Provider provider)
     {
-        try
-        {
-            using (var db = new PetaPoco.Database("linkSTEM"))
-            {
-                db.Insert("Providers", "provider_ID", provider);
-                return true;
-            }
-
-        } catch (Exception)
-        {
-            return false;
-        }
-        
+        return this.dbProvider.addProvider(provider);        
     }
 
     [WebMethod]
-    public static string getProvider(string username, string password)
+    public string getProvider(string username, string password)
     {
-        using (var db = new PetaPoco.Database("linkSTEM"))
-        {
-            var SQL = Sql.Builder.Append("SELECT *")
-                                 .Append("FROM [dbo].[Providers]")
-                                 .Append("WHERE [provider_Username] = @0 AND [provider_Password] = @1", username, password);
-
-            return JsonConvert.SerializeObject(db.FirstOrDefault<Provider>(SQL));
-        }
+        return this.dbProvider.getProvider(username, password);
     }
 
     [WebMethod]
-    public static string getProviderMarkers(int providerID)
+    public string getProviderMarkers(int providerID)
     {
-        using (var db = new PetaPoco.Database("linkSTEM"))
-        {
-            var SQL = Sql.Builder.Append("SELECT *")
-                                 .Append("FROM [dbo].[Markers]")
-                                 .Append("WHERE [provider_ID] = @0", providerID);
-
-            return JsonConvert.SerializeObject(db.Fetch<Marker>(SQL));
-        }
+        return this.getProviderMarkers(providerID);
     }
 }
