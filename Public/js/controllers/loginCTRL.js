6fc89d033661687cc47066959ca6641d66a9869c
sam.j.maclaren <sam.j.maclaren@gmail.com>
2018-02-24 21:49:19 -0500

    merging for stemap

--- Public/js/controllers/loginCTRL.js
+++ Public/js/controllers/loginCTRL.js
@@ -1,7 +1,9 @@
-﻿app.controller('loginCTRL', ['$scope', function ($scope) {
-    $scope.init = function () {
-        console.log("on login page.");
+﻿app.controller('loginCTRL', ['$scope', '$location', function ($scope, $location) {
+    $scope.SignIn = function () {
+        if ($scope.Email && $scope.Password) {
+            $location.path('/profile');
+        } else {
+            alert("No username/password! Please, try again.")
+        }
     }
-
-    $scope.init();
 }]);
