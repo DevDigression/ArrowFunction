fc356d63daca81771872bb7b30899d2976584c3a
Chris Rodgers <devdigression@gmail.com>
2018-02-25 01:12:24 -0500

    added basic display data

--- index.js
+++ index.js
@@ -5,8 +5,43 @@ const GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json";
 const MAP_API_KEY = "AIzaSyCO2JUg6qbtk_gGZRWS78YmABZEgtC95iQ";
 var map, marker, infoWindow, userQuery, userKeyword;
 
+var providers = [
+	{
+		name: "Sally Science",
+		image: "https://thumbs.dreamstime.com/b/friendly-teacher-22068188.jpg",
+		email: "sally@sfsc.com",
+		company: "South Florida Science Center",
+		keywords: ["Technology", "Physics", "Chemistry", "Aeronautics"],
+		address: "4801 Dreher Trail North, West Palm Beach, FL",
+		website: "https://www.sfsciencecenter.org/",
+		services: ["Fields Trips", "Lectures", "Experiments"]
+	},
+	{
+		name: "Ted Technology",
+		image:
+			"https://png.pngtree.com/element_origin_min_pic/16/10/29/97d5546f88c1fa877434748dabb26235.jpg",
+
+		email: "ted@palmbeachtech.org",
+		company: "Palm Beach Tech",
+		keywords: ["Programming", "Engineering", "Math", "Technology"],
+		address: "313 Datura St, Suite 100, West Palm Beach, FL",
+		website: "https://palmbeachtech.org/",
+		services: ["Internships", "Tutoring", "Meetups"]
+	},
+	{
+		name: "Edward Engineer",
+		image:
+			"https://i.pinimg.com/236x/ac/d9/d7/acd9d7df89783e2650a9f76d2c3bd0d0.jpg",
+		email: "info@southfloridamuseum.org",
+		company: "South Florida Museum",
+		keywords: ["Biology", "Chemistry", "Physics"],
+		address: "201 10th St. W Bradenton, FL",
+		website: "http://www.southfloridamuseum.org/",
+		services: ["Tutoring", "Field Trips", "Internships"]
+	}
+];
+
 $(function() {
-	// Type into landing form  [Map]
 	$("#search").submit(function(event) {
 		event.preventDefault();
 		userQuery = $("#search-query").val();
@@ -28,7 +63,48 @@ $(function() {
 		geoCode(geoParams);
 		// findUser(userSearchParams);
 	});
+
+	var providersList = providers.map(provider => renderProviders(provider));
+
+	$(".providers").html(providersList);
+
+	$(".provider").on("click", "span", function(event) {
+		var providerAddress = $(this).text();
+		const providerParams = {
+			address: providerAddress,
+			key: "AIzaSyCO2JUg6qbtk_gGZRWS78YmABZEgtC95iQ"
+		};
+		geoCode(providerParams);
+	});
+
+	$(".services").on("click", function(event) {
+		$(this)
+			.find("ul")
+			.toggleClass("hidden");
+	});
 });
+
+function renderProviders(provider) {
+	return `
+		<div class="provider">
+		<img src="${provider.image}" />
+		<h2>${provider.name}</h2>
+		<h3>${provider.company}</h3>
+		<p>${provider.email}</p>
+		<p><span>${provider.address}</span></p>
+		<a href=${provider.website} target="_blank">${provider.website}</a>
+		<div class="services">Services
+			<ul class="hidden">
+				${provider.services
+					.map(service => {
+						return `<li>${service}</li>`;
+					})
+					.join("")}
+			</ul>
+		</div>
+		</div>
+      `;
+}
 
 function findUser(params) {
 	$.ajax({
@@ -37,7 +113,6 @@ function findUser(params) {
 		data: params,
 		success: function(data) {
 			var lat = data.results[0].geometry.location.lat;
-			console.log(lat);
 			var lng = data.results[0].geometry.location.lng;
 			const nearbyParams = {
 				key: "AIzaSyCO2JUg6qbtk_gGZRWS78YmABZEgtC95iQ",
@@ -72,7 +147,7 @@ function initMap(data) {
 		lng: data.results[0].geometry.location.lng
 	};
 	map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 4,
+		zoom: 12,
 		center: centerLocation
 	});
 	// marker = new google.maps.Marker({
